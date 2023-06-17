1dd49ad9ec9482553dfd64c760785a66995e130b
krishnaduttPanchagnula <krishnadutt123@gmail.com>
2023-06-17 22:52:35 +0530

    added version code

--- internal/commands/default.go
+++ internal/commands/default.go
@@ -1,16 +1,21 @@
 package commands
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/snyk/parlay/internal/commands/deps"
 	"github.com/snyk/parlay/internal/commands/ecosystems"
 	"github.com/snyk/parlay/internal/commands/snyk"
-	"github.com/snyk/parlay/internal/commands/version"
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+)
+
+// These values are set at build time
+var (
+	version = ""
 )
 
 func NewDefaultCommand() *cobra.Command {
@@ -18,6 +23,7 @@ func NewDefaultCommand() *cobra.Command {
 		Use:                   "parlay",
 		Short:                 "Enrich an SBOM with context from third party services",
 		SilenceUsage:          true,
+		Version:               GetVersion(),
 		DisableFlagsInUseLine: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			_ = cmd.Help()
@@ -34,14 +40,17 @@ func NewDefaultCommand() *cobra.Command {
 
 	cmd.PersistentFlags().Bool("debug", false, "")
 	viper.BindPFlag("debug", cmd.PersistentFlags().Lookup("debug")) //nolint:errcheck
-
+	cmd.SetVersionTemplate(`{{.Version}}`)
 	output := zerolog.ConsoleWriter{Out: os.Stderr}
 	logger := zerolog.New(output).With().Timestamp().Logger()
 
 	cmd.AddCommand(ecosystems.NewEcosystemsRootCommand(logger))
-	cmd.AddCommand(version.GetVersion(logger))
 	cmd.AddCommand(snyk.NewSnykRootCommand(logger))
 	cmd.AddCommand(deps.NewDepsRootCommand(logger))
 
 	return &cmd
+}
+
+func GetVersion() string {
+	return fmt.Sprintf("Parley Version : %s", version)
 }
