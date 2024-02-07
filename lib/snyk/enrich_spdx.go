e5db958c9ddc760d6f4052647344e5780f21c075
Maximilian Combüchen <max.combuchen@snyk.io>
2024-02-07 14:29:32 +0100

    fix: log failure reasons during `snyk enrich` (#54)

--- lib/snyk/enrich_spdx.go
+++ lib/snyk/enrich_spdx.go
@@ -38,15 +38,13 @@ const (
 func enrichSPDX(bom *spdx.Document, logger zerolog.Logger) *spdx.Document {
 	auth, err := AuthFromToken(APIToken())
 	if err != nil {
-		// TODO: log error when logger instance available.
-		// See https://github.com/snyk/parlay/pull/49
+		logger.Fatal().Err(err).Msg("Failed to authenticate.")
 		return nil
 	}
 
 	orgID, err := SnykOrgID(auth)
 	if err != nil {
-		// TODO: log error when logger instance available.
-		// See https://github.com/snyk/parlay/pull/49
+		logger.Error().Err(err).Msg("Failed to infer preferred org.")
 		return nil
 	}
 
